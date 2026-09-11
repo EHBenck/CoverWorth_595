4df1785f0f35f04b4bd49e7a02c2fc7bd3362efa
Jacob <jvuko147@gmail.com>
2026-09-11 10:30:51 -0500

    Set up Django backend

---
+++ backend/coverworth_backend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for coverworth_backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coverworth_backend.settings")
+
+application = get_wsgi_application()
